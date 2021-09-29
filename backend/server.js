c214d0294e5f2a1bc1ed490ee35e71be0bdc86a1
Sandun01 <sandunlakshitha06@gmail.com>
2021-09-29 18:47:22 +0530

    completed backed procurement functions

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from './src/config/db.js';
 
 import userRoute from './src/routes/userRoutes.js';
 import SiteRoutes from './src/routes/SiteRoutes.js';
+import PurchaseOrderRoutes from './src/routes/PurchaseOrderRoutes.js'
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 //routes
 app.use('/api/users', userRoute);
 app.use('/api/sites', SiteRoutes);
+app.use('/api/purchase_orders', PurchaseOrderRoutes);
 
 const PORT = process.env.PORT || 5000;
 
